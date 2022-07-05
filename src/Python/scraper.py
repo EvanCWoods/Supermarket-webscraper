982de89fefb3b4709c9ad8691dc0923572c5e965
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-07-05 20:12:37 +1000

    got selenium to work to bypass dynamic content blockers

--- src/Python/scraper.py
+++ src/Python/scraper.py
@@ -1,27 +1,9 @@
-from bs4 import BeautifulSoup
-import requests
-import re
+from selenium import webdriver
 
-section = "https://shop.coles.com.au/a/national/triple-flybuys-points-selection/browse/meat-seafood/beef-veal?pageNumber=1"
-
-def getHTMLdocument(url):    
-    # request for HTML document of given url
-    response = requests.get(url)
-    # response will be provided in JSON format
-    return response.text
-
-
-def runScrape():
-    html_document = getHTMLdocument(section)
-    # create soap object
-    soup = BeautifulSoup(html_document, 'html.parser')
-    for a in soup.find_all("a"):
-        print(a.href)
-    print(soup)
-
-
-def main():
-    runScrape()
-
-if __name__=="__main__":
-    main()
+driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+global_dynamicUrl = "https://iqssdss2020.pythonanywhere.com/tutorial/default/dynamic"
+driver.get(global_dynamicUrl)
+table_area = driver.find_element_by_xpath('//*[@id="result"]/table')
+table_entries = table_area.find_elements_by_tag_name("tr")
+print(len(table_entries))
+driver.close()
