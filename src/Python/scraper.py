ce6a432df8bf0577bf6212b90ae202e5c7e460d3
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-07-12 19:44:43 +1000

    function to return the beautiful soup parsed html

--- src/Python/scraper.py
+++ src/Python/scraper.py
@@ -3,22 +3,23 @@ from bs4 import BeautifulSoup
 
 # path to chromedriver
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')
-# url to emulate using selenium
-global_dynamicUrl = "https://www.woolworths.com.au/shop/search/products?searchTerm=beef"
-# open the emulator
-driver.get(global_dynamicUrl)
-html = driver.page_source
 
 # Function to get the user input
 def getData():
     userChoice = input("What are you searching for? ")
     return userChoice
 
-# Start parsing the emulated content using BeautifulSoup
-soup = BeautifulSoup(html)
+# Function to search woolworths using the search
+def searchWoolworths():
+    # url to emulate using selenium=
+    searchUrl = f"https://www.woolworths.com.au/shop/search/products?searchTerm={getData()}"
+    # open the emulator
+    driver.get(searchUrl)
+    # Start parsing the emulated content using BeautifulSoup
+    return BeautifulSoup(driver.page_source)
 
 # Isolate the dollar prices
-for tag in soup.find_all('span', class_="price-dollars"):
+for tag in searchWoolworths().find_all('span', class_="price-dollars"):
     print(tag.text)
 
 # close the connection to selenium // NEEDED
