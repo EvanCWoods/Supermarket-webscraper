8fd241b6c697906757e4f6419e7371014c087ae4
Evan Woods <84051498+EvanCWoods@users.noreply.github.com>
2022-07-24 21:26:37 +1000

    removed redundant code

--- src/Python/scraper.py
+++ src/Python/scraper.py
@@ -7,28 +7,15 @@ driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 chrome_options = Options()
 # chrome_options.add_argument("--headless")
 
-searchUrls = ["https://www.woolworths.com.au/shop/search/products?searchTerm=", "https://shop.coles.com.au/a/national/everything/search/"]
+searchUrls = [
+    "https://www.woolworths.com.au/shop/search/products?searchTerm=", 
+    "https://shop.coles.com.au/a/national/everything/search/"
+    ]
+    
 # Function to get the user input
 def getData():
     userChoice = input("What are you searching for? ")
     return userChoice
-
-# Function to search woolworths using the search
-def searchStores():
-    # Load the user choice in to memory once
-    userChoice = getData()
-    # url to emulate using selenium=
-    for item in searchUrls:
-        # open the emulator
-        driver.get(item + userChoice)
-        # Start parsing the emulated content using BeautifulSoup
-            # second argument is for the parsing method,
-                # should be looked in to
-    return BeautifulSoup(driver.page_source, features="lxml")
-
-# Isolate the dollar prices
-# for tag in searchStores().find_all('span', class_="price-dollars"):
-#     print(tag.text)
 
 
 # Function to run the searches recursively
@@ -39,20 +26,16 @@ def getAllData():
         returnPrices(BeautifulSoup(driver.page_source, features="lxml"))
 
 
-
 # function to print the prices
 def returnPrices(pageSource):
     for tag in pageSource.find_all('span', class_="price-dollars"):
         print(tag.text)
 
 
-
-
 def main():
     getAllData()
+    driver.close()
 
 
 if __name__=="__main__":
     main()
-# # close the connection to selenium // NEEDED
-# driver.close()
